Patternslib/Patterns

commit be33d94ea162419eeb88a0c676929ba7a3d031a4

Allow specifying the auto-scale method.

src/patterns/autoscale.js
@@ -1,44 +1,62 @@
 define([
     "jquery",
-    "../registry"
-], function($, registry) {
+    "../registry",
+    "../core/parser"
+], function($, registry, Parser) {
+    var parser = new Parser("auto-scale");
+    parser.add_argument("method", "scale", ["scale", "zoom"]);
+
     var _ = {
         name: "autoscale",
         trigger: ".pat-auto-scale",
-        method: "scale",
+        force_method: null,
 
         _setup: function() {
-            if ($.browser.msie && parseInt($.browser.version, 10)<10)
-                _.method="zoom";
+            if ($.browser.mozilla)
+                _.force_method="scale";
+            else if ($.browser.msie && parseInt($.browser.version, 10)<10)
+                _.force_method="zoom";
             $(window).on("resize.autoscale", _.onResize);
         },
 
-        init: function($el, options) {
-            return $el.each(_.resizeElement);
+        init: function($el, opts) {
+            return $el.each(function() {
+                var $el = $(this);
+                    method = _.force_method;
+                if (method===null) {
+                    var options = _._parser.parse($el, opts);
+                    method=options.method;
+                    $el.data("patterns.auto-scale", method);
+                }
+                _.scale.apply(this, []);
+            });
         },
 
-        resizeElement: function() {
-            var $this = $(this),
+        scale: function() {
+            var $el = $(this),
+                method = _.force_method,
                 scale;
 
-            if (this.tagName.toLowerCase()==='body')
-                scale = $(window).width()/$this.outerWidth();
+            if ($el[0].tagName.toLowerCase()==='body')
+                scale = $(window).width()/$el.outerWidth();
             else
-                scale = $this.parent().outerWidth()/$this.outerWidth();
+                scale = $el.parent().outerWidth()/$el.outerWidth();
 
-            switch (_.method) {
+            if (method===null)
+                method=$el.data("patterns.auto-scale");
+            switch (method) {
             case "scale":
-                $this.css('transform', 'scale(' + scale + ')');
+                $el.css('transform', 'scale(' + scale + ')');
                 break;
             case "zoom":
-                $this.css('zoom', scale);
+                $el.css('zoom', scale);
                 break;
             }
-            $this.addClass("scaled");
+            $el.addClass("scaled");
         },
 
         onResize: function() {
-            $(_.trigger).each(_.resizeElement);
+            $(_.trigger).each(_.scale);
         }
     };
 

tests/autoscale.js
@@ -12,39 +12,49 @@ describe("bumper-pattern", function() {
     });
 
     describe("setup", function() {
-        var original_method, msie, version;
+        var force_method, mozilla, msie, version;
 
         beforeEach(function() {
-            original_method=pattern.method;
-            pattern.method="scale";
+            force_method=pattern.force_method;
+            mozilla=jQuery.browser.mozilla;
             msie=jQuery.browser.msie;
             version=jQuery.browser.version;
+            pattern.force_method=null;
+            jQuery.browser.mozilla=false;
+            jQuery.browser.msie=false;
         });
 
         afterEach(function() {
-            pattern.method=original_method;
+            pattern.force_method=force_method;
+            jQuery.browser.mozilla=mozilla;
             jQuery.browser.msie=msie;
             jQuery.browser.version=version;
         });
 
-        it("Use zoom on old IE versions", function() {
+        it("Force zoom on old IE versions", function() {
             jQuery.browser.msie=true;
             jQuery.browser.version="9.192.921";
             pattern._setup();
-            expect(pattern.method).toBe("zoom");
+            expect(pattern.force_method).toBe("zoom");
         });
 
-        it("Use scale on recent IE versions", function() {
+        it("Force nothing on recent IE versions", function() {
             jQuery.browser.msie=true;
             jQuery.browser.version="10.0.19A";
             pattern._setup();
-            expect(pattern.method).toBe("scale");
+            expect(pattern.force_method).toBe(null);
         });
 
-        it("Use scale on non-IE browsers", function() {
-            jQuery.browser.msie=false;
+        it("Force scale on gecko", function() {
+            // See https://bugzilla.mozilla.org/show_bug.cgi?id=390936
+            jQuery.browser.mozilla=true;
             pattern._setup();
-            expect(pattern.method).toBe("scale");
+            expect(pattern.force_method).toBe("scale");
+        });
+
+        it("Force nothing on other browsers", function() {
+            pattern._setup();
+            expect(pattern.force_method).toBe(null);
         });
     });
 
@@ -56,24 +66,16 @@ describe("bumper-pattern", function() {
         });
     });
 
-    describe("resizeElement", function() {
+    describe("scale", function() {
         var original_method;
-
-        beforeEach(function() {
-            original_method=pattern.method;
-        });
-
-        afterEach(function() {
-            pattern.method=original_method;
-        });
 
         it("Scale element", function() {
             $("<div/>", {id: "parent"}).css({width: "200px"})
               .append($("<div/>", {id: "child"}).css({width: "50px"}))
               .appendTo("#lab");
             var child = document.getElementById("child");
-            pattern.method="scale";
-            pattern.resizeElement.apply(child, []);
+            $(child).data("patterns.auto-scale", "scale");
+            pattern.scale.apply(child, []);
             expect(child.getAttribute("style")).toMatch(/transform: scale\(4\);/);
         });
 
@@ -82,8 +84,8 @@ describe("bumper-pattern", function() {
               .append($("<div/>", {id: "child"}).css({width: "50px"}))
               .appendTo("#lab");
             var child = document.getElementById("child");
-            pattern.method="zoom";
-            pattern.resizeElement.apply(child, []);
+            $(child).data("patterns.auto-scale", "zoom");
+            pattern.scale.apply(child, []);
             expect(child.style.zoom).toBe("4");
         });
 
@@ -92,7 +94,8 @@ describe("bumper-pattern", function() {
               .append($("<div/>", {id: "child"}).css({width: "50px"}))
               .appendTo("#lab");
             var child = document.getElementById("child");
-            pattern.resizeElement.apply(child, []);
+            $(child).data("patterns.auto-scale", "zoom");
+            pattern.scale.apply(child, []);
             expect($(child).hasClass("scaled")).toBeTruthy();
         });
     });

Patternslib/Patterns-archive

commit 290d3ac6f3e63c278cb4f283593cd529a16dac61

Fix auto-scale initialisation
be33d94ea162419eeb88a0c676929ba7a3d031a4 broke the parser setup :(

src/patterns/autoscale.js
@@ -24,7 +24,7 @@ define([
                 var $el = $(this);
                     method = _.force_method;
                 if (method===null) {
-                    var options = _._parser.parse($el, opts);
+                    var options = parser.parse($el, opts);
                     method=options.method;
                     $el.data("patterns.auto-scale", method);
                 }

tests/autoscale.js
@@ -64,6 +64,19 @@ describe("bumper-pattern", function() {
             jq.each.andReturn(jq);
             expect(pattern.init(jq)).toBe(jq);
         });
+
+	it("Perform initial scaling", function() {
+            $("<div/>", {id: "parent"}).css({width: "200px"})
+              .append($("<div/>", {id: "child", "data-pat-auto-scale": "scale"})
+                  .css({width: "50px"}))
+              .appendTo("#lab");
+            var $child = $("#child");
+            pattern.init($child);
+            spyOn(pattern, "scale");
+            pattern.init($child);
+            expect($child.data("patterns.auto-scale")).toBe("scale");
+            expect(pattern.scale).toHaveBeenCalled();
+	});
     });
 
     describe("scale", function() {